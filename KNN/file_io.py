db0cb26336b625b93379ee0548e09af63f96a838
Luke Runnels <lar9482@utulsa.edu>
2023-01-17 22:53:17 -0600

    Implemented 'load_labeled_examples' for analysis

--- KNN/file_io.py
+++ KNN/file_io.py
@@ -1,8 +1,16 @@
 import os
 import sys
 
-import 
+from Label_Item import Label_Item
 
 def load_labeled_examples():
+    labeled_item_list = []
     with open(os.path.join(sys.path[0], "labeled-examples.txt"), "r") as f:
-        print(f.read())
+        while True:
+            content = f.readline().split(' ')
+            if content[0] == '':
+                break
+            
+            labeled_item_list.append(Label_Item(int(content[0]), float(content[1]), float(content[2]), content[3].replace('\n', '')))
+    
+    return labeled_item_list
--- KNN/main.py
+++ KNN/main.py
@@ -4,7 +4,7 @@ from file_io import load_labeled_examples
 def main():
     label1 = Label_Item(0, 0.5, 0.5, "item1")
     label2 = Label_Item(0, 0.6, 0.6, "item2")
-    load_labeled_examples()
+    list = load_labeled_examples()
     print()
 
 if __name__ == "__main__":
