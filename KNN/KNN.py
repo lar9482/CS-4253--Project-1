7eaef5a92f5672e429818f4872884cd81865f478
Luke Runnels <lar9482@utulsa.edu>
2023-01-18 12:29:40 -0600

    Wrote comments for the 'classify' method.

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -39,22 +39,35 @@ class KNN:
     def classify(self, item):
         distance_item = {}
         # distance_item: {distance: item}
+
+        #First, calculate all of the distances in the stored set relative to the inputted item
         for stored_item in self.store:
             distance = self.calc_distance(item, stored_item)
             distance_item[distance] = stored_item
         
+        #Grab all of the distances and sort them from least to greatest
         allDistances = list(distance_item.keys())
         allDistances.sort()
 
         #className_to_distanceCount {class_name: count}
         className_to_distanceCount = {}
+
+        #Scanning the distances for the kth smallest distances
         for index in range(0, self.k):
+
+            #Get an item whose distance from the inputted item is one of the lowest
             close_item = distance_item[allDistances[index]]
+
+            #If no key-pair from the class name and the number of items that have been detected has been created yet.
             if className_to_distanceCount.get(close_item.class_name) is None:
+                #Initialize key-pair from class name and the number of items that have been detected.
                 className_to_distanceCount[close_item.class_name] = 1
             else:
+                #Increment key-pair
                 className_to_distanceCount[close_item.class_name] += 1
         
+        # Return the key(class_name) whose value(item count) is the greatest, 
+        # indicating the classification of the inputted item
         return max(className_to_distanceCount, key=className_to_distanceCount.get)
         
 
