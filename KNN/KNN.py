d10377767b58c708680480aea97eb1bd8d7c2f31
Luke Runnels <lar9482@utulsa.edu>
2023-01-17 23:44:06 -0600

    Wrote placeholder objects in the params for debugging later.

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -1,10 +1,21 @@
+from Item import Item
+
+placeholder_dataset = [Item(0, [0, 0])]
+placeholder_item = Item(0, [0, 0])
+
 class KNN:
-    def __init__(self, k = 10):
+    def __init__(self, k = 15):
+        if k % 2 != 0:
+            raise Exception("The inputted k must be odd")
+            
         self.k = k
         self.store = {}
 
-    def train(self, train_dataset, store_all = True):
+    def train(self, train_dataset = placeholder_dataset, store_all = True):
         pass
 
-    def eval(self, test_dataset):
+    def eval(self, test_dataset = placeholder_dataset):
+        pass
+
+    def calc_distance(self):
         pass
