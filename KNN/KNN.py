c6b7af93254a6db74e58452ca59e2c9b75cb06c0
Luke Runnels <lar9482@utulsa.edu>
2023-01-17 23:33:01 -0600

    Wrote function outlines for the KNN implementation.

---
+++ KNN/KNN.py
@@ -0,0 +1,10 @@
+class KNN:
+    def __init__(self, k = 10):
+        self.k = k
+        self.store = {}
+
+    def train(self, train_dataset, store_all = True):
+        pass
+
+    def eval(self, test_dataset):
+        pass
