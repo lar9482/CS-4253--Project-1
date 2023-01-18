139803302df5c29f74651ffbc7917b5bebc510f3
Luke Runnels <lar9482@utulsa.edu>
2023-01-17 20:03:04 -0600

    Making sure Label_item class can be initialized

--- KNN/Label_Item.py
+++ KNN/Label_Item.py
@@ -1,6 +1,6 @@
-import Item
+from Item import Item
 
 class Label_Item(Item):
     def __init__(self, class_name, x, y, item_name):
-        super.__init__(class_name, [x, y])
+        super().__init__(class_name, [x, y])
         self.item_name = item_name
---
+++ KNN/main.py
@@ -0,0 +1,9 @@
+from Label_Item import Label_Item
+
+def main():
+    label1 = Label_Item(0, 0.5, 0.5, "item1")
+    label2 = Label_Item(0, 0.6, 0.6, "item2")
+    print()
+
+if __name__ == "__main__":
+    main()
