bcdc10f10632829f9b722ce515dae47013ed689b
Luke Runnels <lar9482@utulsa.edu>
2023-01-18 09:47:55 -0600

    Basic implementation for 'store all' training.

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -5,14 +5,21 @@ placeholder_item = Item(0, [0, 0])
 
 class KNN:
     def __init__(self, k = 15):
-        if k % 2 != 0:
+        if k % 2 == 0:
             raise Exception("The inputted k must be odd")
-            
+
         self.k = k
         self.store = {}
 
     def train(self, train_dataset = placeholder_dataset, store_all = True):
-        pass
+        if store_all:
+            for item in train_dataset:
+                if self.store.get(item.class_name) is None:
+                    self.store[item.class_name] = [item]
+                else:
+                    self.store[item.class_name].append(item)
+        else:
+            pass
 
     def eval(self, test_dataset = placeholder_dataset):
         pass
--- KNN/main.py
+++ KNN/main.py
@@ -1,10 +1,14 @@
 from Label_Item import Label_Item
 from file_io import load_labeled_examples
+from KNN import KNN
 
 def main():
     label1 = Label_Item(0, 0.5, 0.5, "item1")
     label2 = Label_Item(0, 0.6, 0.6, "item2")
     list = load_labeled_examples()
+    model = KNN()
+
+    model.train(list)
     print()
 
 if __name__ == "__main__":
