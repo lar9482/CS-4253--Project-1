25b20a0d7fb91d0df5430943606ef3ebb30e31ae
Luke Runnels <lar9482@utulsa.edu>
2023-01-19 13:43:36 -0600

    Running some initial tests for N Fold

--- KNN/N_Fold.py
+++ KNN/N_Fold.py
@@ -1,6 +1,27 @@
-
-
 from KNN import KNN
 
-def N_Fold(dataset, n = 5, k = 15):
-    print()
+def N_Fold(dataset, n = 5, k = 15, storeAll = True):
+    if (len(dataset) % n != 0):
+        raise Exception("Incompatible n: The inputted n parameter must be able to evenly divide " + 
+                        "the dataset.")
+    startIndex = 0
+    endIndex = n
+
+    accu_train = 0
+    accu_test = 0
+    while (endIndex < len(dataset)):
+        dataset_train = dataset[0:startIndex:1] + dataset[endIndex:len(dataset):1]
+        dataset_test = dataset[startIndex:endIndex:1]
+
+        model = KNN(k)
+        model.train(dataset_train, storeAll)
+
+        accu_train += model.eval(dataset_train)
+        accu_test += model.eval(dataset_test)
+
+        startIndex = endIndex
+        endIndex += n
+        print(startIndex)
+        
+
+    return ((accu_train / (n)), (accu_test / (n)))
--- KNN/main.py
+++ KNN/main.py
@@ -1,14 +1,17 @@
 from Label_Item import Label_Item
 from file_io import load_labeled_examples
 from KNN import KNN
+from N_Fold import N_Fold
 
 def main():
 
-    k = 15
+    k = 131
+    n = 5
     list = load_labeled_examples()
-    model = KNN(k)
-    model.train(list, False)
-    accu = model.eval(list)
+    (a, b) = N_Fold(list, n, k, False)
+    print(a)
+    print(b)
+
     
     #model.classify(label1)
 
