7b92e2ab3992b0146ffd3459954290dd0990bfbd
Luke Runnels <lar9482@utulsa.edu>
2023-01-18 18:17:58 -0600

    Wrote some code comments for the functions.

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -19,15 +19,22 @@ class KNN:
         else:
             storedClasses_soFar = {}
             #className_to_distanceCount {class_name: count}
+
             for item in train_dataset:
+
+                #If a key-value pair between an class name and count hasn't been
+                #established yet.
                 if (storedClasses_soFar.get(item.class_name) is None):
                     storedClasses_soFar[item.class_name] = 1
 
+                #If the count of the class name is greater than k, and the current is a misclassification,
+                #then store it too.
                 elif storedClasses_soFar[item.class_name] > self.k:
                     if self.classify(item) != item.class_name:
                         storedClasses_soFar[item.class_name] += 1
                         self.store.append(item)
 
+                #If count is class name is less than k, then just store the item.
                 else:
                     storedClasses_soFar[item.class_name] += 1
                     self.store.append(item)
@@ -84,6 +91,7 @@ class KNN:
         if len(item1.attributes) != len(item2.attributes):
             raise Exception("Incompatible items: Both items need the same number of attributes")
         
+        #Calculate the euclidean distance between the attributes of the two items.
         distance = 0
         for i in range(0, len(item1.attributes)):
             distance += (item1.attributes[i] - item2.attributes[i]) ** 2
