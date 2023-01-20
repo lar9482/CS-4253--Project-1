80dfff3b590b1d951ca9d3ef8c5e7c257b7c465d
Luke Runnels <lar9482@utulsa.edu>
2023-01-20 17:21:14 -0600

    Introducing concurrency to the program. Each thread will run one instance of 'N_Fold' on a different n and k pair.

--- KNN/main.py
+++ KNN/main.py
@@ -4,28 +4,33 @@ from KNN import KNN
 from N_Fold import N_Fold
 
 import random
+import threading
 
-def run_labeled_examples(kMin, kMax, nMin, nMax, store_all = True, shuffle = True):
+def N_Fold_labeled_examples(k, n, store_all = True, shuffle = True):
     dataset = load_labeled_examples()
     if (shuffle):
         random.shuffle(dataset)
+    (train_accu, test_accu) = N_Fold(dataset, n, k, store_all)
+    print("Average Training Accuracy: %s" % (train_accu))
+    print("Average Testing Accuracy: %s" % (test_accu))
+    print()
 
+def run_labeled_examples(kMin, kMax, nMin, nMax, store_all = True, shuffle = True):
+    iteration = 0
     for n in range(nMin, nMax):
         for k in range(kMin, kMax):
             if (k % 2 == 0):
                 continue
 
-            (train_accu, test_accu) = N_Fold(dataset, n, k, store_all)
-            print("Average Training Accuracy: %s" % (train_accu))
-            print("Average Testing Accuracy: %s" % (test_accu))
-            print()
+            thread = threading.Thread(target=N_Fold_labeled_examples, args=(k, n, store_all, shuffle))
+            thread.start()
 
 def main():
     kMin = 5
-    kMax = 25
+    kMax = 15
     nMin = 5
-    nMax = 25
-    store_all = True
+    nMax = 10
+    store_all = False
     shuffle = True
     run_labeled_examples(kMin, kMax, nMin, nMax, store_all, shuffle)
 
