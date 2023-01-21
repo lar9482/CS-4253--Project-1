f0a50861acf53ddd5ca5d9142873eaf2b05c6aeb
Luke Runnels <lar9482@utulsa.edu>
2023-01-20 18:28:54 -0600

    Switched to using the 'multi-processing' module. Significant speedup using this.

--- KNN/main.py
+++ KNN/main.py
@@ -4,35 +4,49 @@ from KNN import KNN
 from N_Fold import N_Fold
 
 import random
-import threading
+import time
+
+from multiprocessing import Process
 
 def N_Fold_labeled_examples(k, n, store_all = True, shuffle = True):
+
     dataset = load_labeled_examples()
     if (shuffle):
         random.shuffle(dataset)
     (train_accu, test_accu) = N_Fold(dataset, n, k, store_all)
     print("Average Training Accuracy: %s" % (train_accu))
     print("Average Testing Accuracy: %s" % (test_accu))
-    print()
 
-def run_labeled_examples(kMin, kMax, nMin, nMax, store_all = True, shuffle = True):
-    iteration = 0
-    for n in range(nMin, nMax):
+def concurrent_run_labeled_examples(kMin, kMax, nMin, nMax, store_all = True, shuffle = True):
+    start_time = time.time()
+    all_processes = []
+    for n in range(nMin, nMax+1):
         for k in range(kMin, kMax):
             if (k % 2 == 0):
                 continue
 
-            thread = threading.Thread(target=N_Fold_labeled_examples, args=(k, n, store_all, shuffle))
-            thread.start()
+            process = Process(target=N_Fold_labeled_examples, args=(k, n, store_all, shuffle))
+            all_processes.append(process)
+            
+            
+    
+    for process in all_processes:
+        process.start()
+    
+    for process in all_processes:
+        process.join()
+
+    print(time.time() - start_time)
+    print()
 
 def main():
     kMin = 5
-    kMax = 15
+    kMax = 25
     nMin = 5
     nMax = 10
     store_all = False
     shuffle = True
-    run_labeled_examples(kMin, kMax, nMin, nMax, store_all, shuffle)
+    concurrent_run_labeled_examples(kMin, kMax, nMin, nMax, store_all, shuffle)
 
 if __name__ == "__main__":
     main()
