c4c5e13dc4422bf1ba17890cf4a1f45c1cdb3308
Luke Runnels <lar9482@utulsa.edu>
2023-01-29 01:19:33 -0600

    Comments to describe the goal of each of the scheduling functions

--- Local_Search/SA/schedule.py
+++ Local_Search/SA/schedule.py
@@ -1,14 +1,17 @@
 import math
 
 #A bunch of additive cooling functions 
-# def exponential_schedule(t, T_0 = 1000, T_N = 0, n = 10):
-#     return (T_0)*(alpha ** t)
+#Source:
+#http://what-when-how.com/artificial-intelligence/a-comparison-of-cooling-schedules-for-simulated-annealing-artificial-intelligence/
 
-def linear_schedule(t, T_0 = 1000, T_N = 0, n = 2):
+#Either bias toward exploitation or exploration
+def linear_schedule(t, T_0 = 1000, T_N = 0, n = 20):
     return (T_N) + (T_0 - T_N)*((n - t) / (n))
 
-def quadratic_schedule(t, T_0 = 1000, T_N = 0, n = 2):
+#Bias toward exploitation
+def quadratic_schedule(t, T_0 = 1000, T_N = 0, n = 20):
     return (T_N) + (T_0 - T_N)*(((n - t) / (n)) ** 2)
 
+#Bias toward exploration
 def trigonometric_schedule(t, T_0 = 1000, T_N = 0, n = 2):
     return (T_N) + 0.5*(T_0 - T_N)*(1 + math.cos((t*math.pi) / (n)))
